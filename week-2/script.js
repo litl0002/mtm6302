adb323091733c3371695e83ed7d415c80292a7e0
Adam Litle <litl0002@algonquinlive.com>
2021-09-15 13:12:57 -0400

    first code

---
+++ week-2/script.js
@@ -0,0 +1,29 @@
+// Single line comment
+
+/* 
+Multi Line Comment 
+*/
+
+const testVariable = 'test' // inline comment
+
+/*
+
+const courseCode = 'mtm6302'    //dont just use a letter, name everything properly ex. coursecode good! cc bad!
+console.log(courseCode)    //can have multiple but need to change at least a letter
+
+*/
+
+// declaration statement
+// var > wont use in this course
+// let
+// const
+
+// variable value will/can change
+let dateToday = 210915
+// console.log(dateToday)
+dateToday = 210916
+// console.log(dateToday)
+
+// values that wont change
+const firstName = 'Adam'
+const earthGravity = 9.8
