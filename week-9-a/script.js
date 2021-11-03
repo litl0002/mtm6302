d802585a4d3efa057d7d9af178028be40cf43fa2
Adam Litle <litl0002@algonquinlive.com>
2021-11-03 12:34:43 -0400

    end of class

---
+++ week-9-a/script.js
@@ -0,0 +1,8 @@
+// https://picsum.photos/id/353/333/333
+const $continer = document.getElementById('container')
+const $image = document.querySelector('img')
+console.log($image.dataset)
+
+console.log($image.dataset.imgSetting)
+console.log($image.dataset.imgSource)
+console.log($image.dataset.imgSize)
